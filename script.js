c719c75b680332053034526925aa5fd2839f9807
ajaythoram <115677014+ajaythoram@users.noreply.github.com>
2023-04-27 14:22:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const list = document.querySelectorAll("ul");
+ let n = list.length;
+ for(let i = 0;i<n;i++){
+   if(list[i].id == "level"){
+      alert("n");
+   }
+ }
